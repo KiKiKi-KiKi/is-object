c9d9e478df69c7e3c0303bc23351187e0d035cc6
KiKiKi <chaika.design@gmail.com>
2023-08-24 06:37:41 +0900

    To TypeScript a tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,75 +1,79 @@
-import isObject from '../src/';
+import isObject from '../src';
 
 describe('isObject', () => {
   test('When empty parameter, return false', () => {
-    expect( isObject() ).toBe(false);
+    expect(isObject()).toBe(false);
   });
 
   test('When param is undefined, return false', () => {
-    expect( isObject(undefined) ).toBe(false);
+    expect(isObject(undefined)).toBe(false);
   });
 
   test('When param is null, return false', () => {
-    expect( isObject(null) ).toBe(false);
+    expect(isObject(null)).toBe(false);
   });
 
   test('When param is NaN, return false', () => {
-    expect( isObject(NaN) ).toBe(false);
+    expect(isObject(NaN)).toBe(false);
   });
 
   test('When param is Boolean, return false', () => {
-    expect( isObject( true ) ).toBe(false);
-    expect( isObject( false ) ).toBe(false);
+    expect(isObject(true)).toBe(false);
+    expect(isObject(false)).toBe(false);
   });
 
   test('When param is function, return false', () => {
-    expect( isObject( function(){ return true } ) ).toBe(false);
+    expect(
+      isObject(function () {
+        return true;
+      }),
+    ).toBe(false);
   });
 
   test('When param is String, return false', () => {
-    expect( isObject( 'String' ) ).toBe(false);
-    expect( isObject( new String('String') ) ).toBe(false);
+    expect(isObject('String')).toBe(false);
+    expect(isObject(new String('String'))).toBe(false);
   });
 
   test('When param is Number, return false', () => {
-    expect( isObject( 1 ) ).toBe(false);
-    expect( isObject( -1 ) ).toBe(false);
+    expect(isObject(1)).toBe(false);
+    expect(isObject(-1)).toBe(false);
   });
 
   test('When param is Array, return false', () => {
-    expect( isObject( [] ) ).toBe(false);
-    expect( isObject( new Array() ) ).toBe(false);
+    expect(isObject([])).toBe(false);
+    expect(isObject(new Array())).toBe(false);
   });
 
   test('When param is Object, return true', () => {
-    expect( isObject( {} ) ).toBe(true);
-    expect( isObject( new Object() ) ).toBe(true);
+    expect(isObject({})).toBe(true);
+    expect(isObject(new Object())).toBe(true);
   });
 
   test('When param is Date, return true', () => {
     // typeof(date) => "object"
     // date.constructor.name => "Date"
     const date = new Date();
-    expect( isObject( date ) ).toBe(false);
+    expect(isObject(date)).toBe(false);
   });
 
   test('When param is RegExp Object, return true', () => {
     // typeof(class) => "object"
     // class.constructor.name => "RegExp"
     const regex1 = /\w+/;
-    expect( isObject( regex1 ) ).toBe(false);
+    expect(isObject(regex1)).toBe(false);
 
     const regex2 = new RegExp('\\w+');
-    expect( isObject( regex2 ) ).toBe(false);
+    expect(isObject(regex2)).toBe(false);
   });
 
   test('When param is Symbol, return false', () => {
     // typeof(symbol) => "symbol"
     const symbol1 = Symbol();
-    expect( isObject( symbol1 ) ).toBe(false);
+    expect(isObject(symbol1)).toBe(false);
 
-    const symbol2 = Symbol( {} );
-    expect( isObject( symbol2 ) ).toBe(false);
+    const symbol2 = Symbol({} as string);
+    expect(isObject(symbol2)).toBe(false);
   });
 
   test('When param is Class, return false', () => {
@@ -77,8 +81,8 @@ describe('isObject', () => {
     // class.constructor.name => "Class Name"
     class MyClass {
       constructor() {}
-    };
-    const classObj =  new MyClass();
-    expect( isObject( classObj ) ).toBe(false);
+    }
+    const classObj = new MyClass();
+    expect(isObject(classObj)).toBe(false);
   });
 });
