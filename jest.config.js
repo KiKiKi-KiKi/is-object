824da452ffdfba24a334e5d4e6e942c0601bf01e
KiKiKi <chaika.design@gmail.com>
2023-08-24 06:34:37 +0900

    Add jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/tests/'],
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ['/node_modules/'],
 };
